004410e7402e77ab7980946bfbaf45afbeb4d144
onnelikvaaal <tatark85@mail.ru>
2023-11-08 14:09:28 +0300

    Initial commit

---
+++ src/main/java/com/example/socksstoreregister/SocksStoreRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.socksstoreregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksStoreRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksStoreRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socksstoreregister/SocksStoreRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socksstoreregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksStoreRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
